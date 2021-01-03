d1d2d940a9e25e11db449a4699f66f142884fc81
ViacheslavKras <emailkras@gmail.com>
2021-01-03 22:33:42 +1000

    Delete serch-panel state

--- src/components/search-panel/search-panel.js
+++ src/components/search-panel/search-panel.js
@@ -1,25 +1,17 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import './search-panel.css';
 
 const SearchPanel = (props) => {
-
-    const [text, setText] = useState('');
-
     const { fromSearchForm } = props;
 
     const searchForm = (e) => {
-        setText(e.target.value);
+        fromSearchForm(e.target.value);
     };
-
-    useEffect(() => {
-        fromSearchForm(text)
-    }, [text, fromSearchForm]);
 
     return (
         <input type='text'
             className='form-control'
             onChange={searchForm}
-            value={text}
         />
     )
 };
